e8ec71cbe945e8c4564a5eebabdeadcba7b44086
M-E-A <miles.e.allen@gmail.com>
2019-07-09 15:42:39 -0400

    Small edits to workings of tmux.

---
+++ TM/interact_tmux.sh
@@ -0,0 +1,105 @@
+#! /bin/bash
+function nl_error {
+  echo "interact: $*" >&2
+  exit 1
+}
+
+export Experiment=Bootstrap
+launch_error=''
+VERBOSE=''
+
+[ -f VERSION ] || nl_error Missing VERSION File
+VERSION=`cat VERSION`
+[ -d "bin/$VERSION" ] || nl_error "Missing bin directory bin/$VERSION"
+export PATH=bin/$VERSION:$PATH
+
+function waitfor {
+  name=$1
+  duration=$2
+  if [ -n "$3" ]; then
+    decrement=$3
+    delay=1
+  else
+    decrement=0
+    delay=5
+  fi
+  while [ $duration -gt 0 ]; do
+    [ -e $name ] && return 0
+    sleep 1
+    let duration=duration-$decrement
+  done
+  return 1
+}
+
+function Launch {
+  name=$1
+  shift
+  [ -n "$launch_error" ] && return 1
+  [ -n "$VERBOSE" ] && msg "[DEBUG] Launch: $*"
+  if { $* & }; then
+    Launch_pid=$!
+    msg "Launch: $Launch_pid $*"
+    # if [ "$name" = "DG/cmd" ]; then
+      # parent_loop="-q -M $Launch_pid -t 5"
+    # fi
+    if [ "$name" != "-" ]; then
+      [ "$name" = "-TMC-" ] && name="/var/run/linkeng/run/$Experiment/$!"
+      [ "${name#/}" = "$name" ] && name="/var/run/linkeng/$Experiment/$name"
+      [ -n "$VERBOSE" ] &&
+        msg "[DEBUG] Launch: Waiting for $Launch_pid:$name"
+      waitfor $name 10 || {
+        msg "[ERROR] Launch: namewait failure: $*"
+        launch_error=yes
+        return 1
+      }
+    fi
+  else
+    msg "[ERROR] Launch: $*"
+    launch_error=yes
+    return 1
+  fi
+  return 0
+}
+
+function Debug {
+  name=$1
+  shift
+  [ -n "$launch_error" ] && return 1
+  [ -n "$VERBOSE" ] && msg "[DEBUG] Debug: $*"
+  program=$1
+  cygstart mintty gdb $program
+  if [ "$name" != "-" -a "$name" != "-TMC-" ]; then
+    [ "${name#/}" = "$name" ] && name="/var/run/linkeng/$Experiment/$name"
+    [ -n "$VERBOSE" ] &&
+      msg "[DEBUG] Debug: Waiting for $name"
+    waitfor $name 1 0
+  fi
+  return 0
+}
+
+function Dispatch_nc {
+  [ "x$1" = "x-" ] && shift 1
+  [ -n "$VERBOSE" ] && msg "[DEBUG] Dispatch_nc: $*"
+  cygstart mintty cyg_nc.sh $*
+}
+
+rm -rf /var/run/linkeng/${Experiment}*
+rm -f $Experiment.log *.stackdump
+# rm -rf LOG
+memoname=/var/run/linkeng/$Experiment/memo
+# ls -l $memoname
+[ -e $memoname ] || {
+  echo "Launching memo for $memoname"
+  cygstart mintty memo -o Bootstrap.log -l2 -V
+  waitfor $memoname 2 || nl_error "Memo launch failed"
+}
+
+Launch      tm_bfr bfr -v
+Launch      -TMC-  lgr -N `mlf_find LOG`
+Launch      -TMC-  Bootstrapengext
+Dispatch_nc -      Bootstrapdispnc -a
+Launch      tm_gen Bootstrapcol -v
+Launch      cmd    Bootstrapsrvr -v
+Launch      -      driver -v
+Launch      -      tmdf
+Dispatch_nc -      Bootstrapcltnc
--- TM/tmux_test.sh
+++ TM/tmux_test.sh
@@ -4,7 +4,13 @@ SESSION='tmux-test'
 
 tmux new-session -n main-window -s $SESSION -d
 tmux send-keys -t $SESSION "tmux new-window -n ancillary-window" C-m
+tmux send-keys -t $SESSION "tmux split-window -v" C-m
+tmux send-keys -t $SESSION "tmux last-pane" C-m
+tmux send-keys -t $SESSION "ls" C-m
 tmux send-keys -t $SESSION "tmux split-window -h" C-m
+tmux send-keys -t $SESSION "ls" C-m #top-left
+tmux send-keys -t $SESSION "tmux split-window -h" C-m
+tmux send-keys -t $SESSION "ls" C-m #bottom
 #tmux send-keys -t $SESSION "tmux bfr -v" C-m
 tmux attach -t $SESSION
 
