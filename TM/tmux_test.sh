b52d711ed343b431f519e3851ed3f5c9b84785f3
M-E-A <miles.e.allen@gmail.com>
2019-09-20 13:22:17 -0400

    tcp_sockets: oops

--- TM/tmux_test.sh
+++ TM/tmux_test.sh
@@ -2,16 +2,10 @@
 
 SESSION='tmux-test'
 
-tmux new-session -n main-window -s $SESSION -d
-tmux send-keys -t $SESSION "tmux new-window -n ancillary-window" C-m
-tmux send-keys -t $SESSION "tmux split-window -v" C-m
-tmux send-keys -t $SESSION "tmux last-pane" C-m
-tmux send-keys -t $SESSION "ls" C-m
-tmux send-keys -t $SESSION "tmux split-window -h" C-m
-tmux send-keys -t $SESSION "ls" C-m #top-left
-tmux send-keys -t $SESSION "tmux split-window -h" C-m
-tmux send-keys -t $SESSION "ls" C-m #bottom
-#tmux send-keys -t $SESSION "tmux bfr -v" C-m
-tmux attach -t $SESSION
+tmux new-session -n main-window -s $SESSION -d bash
 
-#tmux send-keys 'new-window -n ancillary-window'
+tmux send-keys -t $SESSION "cd sw/le-das-integration" C-m
+#tmux send-keys -t $SESSION "tmux new-window -n ancillary-window ./interact_tmux.sh" C-m
+tmux send-keys -t $SESSION "bash ./interact_tmux.sh" C-m
+#tmux send-keys -t $SESSION "tmux show-environment" C-m
+tmux attach -t $SESSION
