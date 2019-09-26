ab5ba8e9517c675e399f509841535024fa383792
M-E-A <miles.e.allen@gmail.com>
2019-09-26 09:30:12 -0400

    tcp_sockets: added tmux_test script

--- TM/tmux_test.sh
+++ TM/tmux_test.sh
@@ -5,7 +5,5 @@ SESSION='tmux-test'
 tmux new-session -n main-window -s $SESSION -d bash
 
 tmux send-keys -t $SESSION "cd sw/le-das-integration" C-m
-#tmux send-keys -t $SESSION "tmux new-window -n ancillary-window ./interact_tmux.sh" C-m
 tmux send-keys -t $SESSION "bash ./interact_tmux.sh" C-m
-#tmux send-keys -t $SESSION "tmux show-environment" C-m
 tmux attach -t $SESSION
